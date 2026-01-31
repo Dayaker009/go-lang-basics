2db4c6d9282d081cd3ea946a1083a633aea075c1
Dayaker009 <dayakerreddy3043@gmail.com>
2026-01-31 22:33:16 +0530

    feat: structs and routines in GO

--- main.go
+++ main.go
@@ -3,13 +3,21 @@ package main // package main.
 import (
 	"fmt"                   // import fmt package
 	"go-lang-basics/helper" // import helper package
-	"strconv"			   // import strconv package
+	"time"
 )
 
 const conferenceName string = "Go Conference" // syntax for declaring and initializing a constant
 var remainingTickets uint = 50
 const conferenceTickets uint = 50
-var bookings = make([]map[string]string, 0) // empty slice of boookings
+var bookings = make([]UserData, 0) // empty slice of boookings
+
+// defining a custom data type using struct
+type UserData  = struct {
+	firstName   string
+	lastName    string
+	email       string
+	userTickets uint
+}
 
 // entry point of the program
 func main() {
@@ -25,13 +33,14 @@ func main() {
 			continue
 		}
 
-		remainingTickets = remainingTickets - userTickets
-
 		bookTicket(userTickets, firstName, lastName, email)
 
-		firstNames := getFirstNames()
+		// go routine to send email, runs concurrently with the main function
+		go sendEmail(userTickets, firstName, lastName, email)
 
-		fmt.Printf("The bookings are: %v\n", firstNames)
+		// firstNames := getFirstNames()
+
+		fmt.Printf("The bookings are: %v\n", bookings)
 
 		if remainingTickets == 0 {
 			println("Our conference is booked out. Come back next year.")
@@ -49,7 +58,7 @@ func greetUsers() {
 func getFirstNames() []string {
 	firstNames := []string{}	
 	for _, booking := range bookings {
-		firstNames = append(firstNames, booking["firstName"]) // appending first name to firstNames slice
+		firstNames = append(firstNames, booking.firstName) // appending first name to firstNames slice
 	}
 	return firstNames
 }
@@ -57,17 +66,26 @@ func getFirstNames() []string {
 func bookTicket(userTickets uint, firstName string, lastName string, email string) {
 
 	// make is a built-in function that allocates and initializes an object of type slice, map, or chan
-	var userData = make(map[string]string) // create a map to store user data
-
-	userData["firstName"] = firstName
-	userData["lastName"] = lastName
-	userData["email"] = email
-	userData["userTickets"] = strconv.FormatUint(uint64(userTickets), 10) // converting uint to string
+	var userData = UserData{
+		firstName: firstName, 
+		lastName: lastName, 
+		email: email, 
+		userTickets: userTickets,
+	} // create a map to store user data
 
 	remainingTickets = remainingTickets - userTickets
 	bookings = append(bookings, userData) // append userData to bookings map list
 	fmt.Printf("Thank you %v for booking %v tickets. You will receive a confirmation email shortly.\n", firstName+" "+lastName, userTickets)
 	fmt.Printf("%v tickets are remaining for %v\n", remainingTickets, conferenceName)
+}
+
+func sendEmail(userTickets uint, firstName string, lastName string, email string) {
+	time.Sleep(10 * time.Second)
+	var emailContent = fmt.Sprintf("Sending %v tickets to %v %v at %v\n", userTickets, firstName, lastName, email)
+	fmt.Println("####################")
+	fmt.Println("Sending email...")
+	fmt.Print(emailContent)
+	fmt.Println("####################")
 }
 
 func handleValidation(firstName string, lastName string, userTickets uint) bool {
